935d156c30f82403110c0c0fba89f677144b9c6e
Samuel <torressam333@gmail.com>
2020-12-03 07:06:48 -0700

    Successfully add a note to notes.json file

--- notes_app/app.js
+++ notes_app/app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const chalk = require('chalk');
 const yargs = require('yargs');
 //Notes utility
-const { notes } = require('./notes');
+const notes  = require('./notes');
 
 //Customize yargs version
 yargs.version('1.1.0');
@@ -27,7 +27,7 @@ yargs.command({
         }
     },
     handler: (argv) => {
-        note.addNote(argv.title, argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 });
 
--- notes_app/notes.js
+++ notes_app/notes.js
@@ -1,9 +1,46 @@
+const fs = require('fs');
+
 const getNotes = function () {
     return 'This is a note... hahaha';
 };
 
 const addNote = function (title, body) {
+    const notes = loadNotes();
 
+    //Look through notes for duplicate titles
+    const duplicateNotes = notes.filter((note) => {
+        return note.title === title
+    });
+
+    if (duplicateNotes.length === 0) {
+        //Push values passed in from app.js
+        notes.push({
+            title: title,
+            body: body,
+        });
+
+        saveNotes(notes);
+        console.log('New note added')
+    } else {
+        console.log('Duplicate note title found');
+    }
+};
+
+//Pass in notes array
+const saveNotes = (notes) => {
+    const jsonData = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', jsonData);
+}
+
+const loadNotes = () => {
+    try {
+        //Returns array of notes
+        const dataBuffer = fs.readFileSync('notes.json');
+        const jsonData = dataBuffer.toString();
+        return JSON.parse(jsonData);
+    } catch (e) {
+        return [];
+    }
 };
 
 module.exports = {
