70bfbe4517397613b0df99163f189b87f7989c6c
Samuel <torressam333@gmail.com>
2020-12-10 07:21:29 -0700

    Set up initial configuration for weather app api call

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,3 +1,8 @@
-console.log('Starting weather app')
+const request = require('postman-request');
 
-console.log('Stopping')
+const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_KEY}&query=37.8267,-122.4233`;
+
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.current);
+})
