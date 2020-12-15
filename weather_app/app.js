aac882fda5790027efccc7cfec37e4738a8a60c4
Samuel <torressam333@gmail.com>
2020-12-15 06:23:57 -0700

    Introduction to weather stack api usage

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,8 +1,13 @@
 const request = require('postman-request');
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_KEY}&query=37.8267,-122.4233`;
+const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_KEY}&query=37.8267,-122.4233&units=f`;
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
-})
+request({
+    url: url,
+    json: true,
+}, (error, response) => {
+    let data = response.body.current;
+
+    console.log(`${data.weather_descriptions[0]} 
+         It is currently ${data.temperature} degrees and it feels like ${data.feelslike} outside.`);
+});
