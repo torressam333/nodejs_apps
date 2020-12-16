83585c3d9a7b3463449722a8d9e45ed5b57f5c20
Samuel <torressam333@gmail.com>
2020-12-15 18:18:21 -0700

    Add error handling for weather stack api call

--- weather_app/app.js
+++ weather_app/app.js
@@ -8,6 +8,28 @@ request({
 }, (error, response) => {
     let data = response.body.current;
 
-    console.log(`${data.weather_descriptions[0]} 
-         It is currently ${data.temperature} degrees and it feels like ${data.feelslike} outside.`);
+    if (error) {
+        console.log('Unable to connect to weather service')
+    } else if (response.body.error) {
+        console.log('Unable to find specified location. Please double check entered address')
+    }else{
+        console.log("The forecast is " + data.weather_descriptions[0] + ". The current temperature is " +
+            data.temperature + " degrees and it feels like " + data.feelslike + " outside.");
+    }
 });
+
+
+/*
+//Api url
+const mapboxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_TOKEN}&limit=1`;
+
+//Api request
+request({
+    url: mapboxUrl,
+    json: true,
+}, (error, response) => {
+    let mapboxData = response.body;
+    let coordinates = mapboxData.features[0].center;
+
+    console.table("Longitude: " + coordinates[0] + " Latitude: " + coordinates[1]);
+});*/
