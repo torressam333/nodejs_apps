c77eb61fec0d6ebdaf5f22b52257ddd596777310
Samuel <torressam333@gmail.com>
2020-12-18 07:10:06 -0700

    Create reusable geocode api call util module

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,5 +1,7 @@
 const request = require('postman-request');
+const geocode = require('./utils/geocodeapi');
 
+/*
 const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_KEY}&query=37.8267,-122.4233&units=f`;
 
 request({
@@ -17,27 +19,10 @@ request({
             data.temperature + " degrees and it feels like " + data.feelslike + " outside.");
     }
 });
+*/
 
 
-
-//Api url
-const mapboxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/chicago.json?access_token=" +
-    process.env.MAPBOX_TOKEN + "&limit=1";
-
-//Api request
-request({
-    url: mapboxUrl,
-    json: true
-}, (error, response) => {
-
-    console.log(response.body);
-    if (error) {
-        console.log('Unable to connect to geolocation services');
-    } else if (response.body.features.length === 0) {
-        console.log('No matching locations found');
-    } else{
-        const latitude = response.body.features[0].center[0];
-        const longitude = response.body.features[0].center[1];
-        console.table("Longitude: " + longitude + " Latitude: " + latitude);
-    }
+geocode('Mecca', (error, data) => {
+    console.log('Error:', error);
+    console.log('Data:', data);
 });
---
+++ weather_app/utils/geocodeapi.js
@@ -0,0 +1,31 @@
+const request = require('postman-request');
+
+//Single Reusable geocode function
+const geocode = (address, callback) => {
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=" +
+        process.env.MAPBOX_TOKEN + "&limit=1";
+
+    request({
+        url: url,
+        json: true
+    }, (error, response) => {
+        //Runs when request completes
+        if (error) {
+            //Send to callback in geocode call
+            callback('Unable to connect to geolocation services', undefined);
+        } else if (response.body.features.length === 0) {
+            callback('No matching locations found', undefined);
+        } else {
+            const weatherData = {
+                latitude: response.body.features[0].center[0],
+                longitude: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            }
+
+            callback(undefined, weatherData);
+        }
+    });
+};
+
+//To be used elsewhere
+module.exports = geocode;
