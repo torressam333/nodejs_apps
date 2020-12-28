51f2098d1b541664c2e5951241e07bdcf6602cf6
Samuel <torressam333@gmail.com>
2020-12-28 12:06:40 -0700

    Integrate html pages and serve up directories using express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,29 +1,30 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const publicDir = path.join(__dirname, '../public');
+
+//Serve up directory(s)
+app.use(express.static(publicDir));
+app.use(express.static(publicDir + '/about.html'));
+app.use(express.static(publicDir + '/help.html'));
 
 /*
 * Configure what the server should do when someone tries to get the resource
 * at a specific URL in our domain.
 * DOCS: https://expressjs.com/en/4x/api.html#app.get
 */
-app.get('', (req, res) => {
-    res.send('Hello express!');
-});
-
-app.get('/help', (req, res) => {
-    res.send('Help Page!');
-});
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About express</h1>');
-});
 
 app.get('/weather', (req, res) => {
-   res.send('<h1>Show Weather</h1>');
+    res.send(
+        {
+            forecast: 'Cloudy with a chance of meatballs',
+            location: 'Buckeye AZ'
+        }
+    );
 });
 
-//Start server (local): http://localhost:3000/
+//Start express server (local): http://localhost:3000/
 app.listen(3000, () => {
     console.log('starting server on port 3000...');
 });
