3f2d5a3044b47c0d8e2e569988aed021bf801422
Samuel <torressam333@gmail.com>
2020-12-30 06:38:48 -0700

    Convert static html files to use handlebars templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,16 +4,38 @@ const path = require('path');
 const app = express();
 const publicDir = path.join(__dirname, '../public');
 
-//Serve up directory(s)
+//Set up Handlebars
+app.set('view engine','hbs');
+
+//Serve up directory for js and css
 app.use(express.static(publicDir));
-app.use(express.static(publicDir + '/about.html'));
-app.use(express.static(publicDir + '/help.html'));
 
 /*
 * Configure what the server should do when someone tries to get the resource
 * at a specific URL in our domain.
 * DOCS: https://expressjs.com/en/4x/api.html#app.get
 */
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Samuel Torres'
+    });
+});
+
+app.get('/about', (req, res) => {
+   res.render('about', {
+       title: 'About',
+       name: 'Samuel Torres'
+   });
+});
+
+app.get('/help', (req, res) => {
+   res.render('help', {
+       title: 'Help Page',
+       helpText: 'Do you need help? If so ensure you have followed all troubleshooting steps prior to contacting one' +
+           'of our agents. Thank you.',
+   });
+});
 
 app.get('/weather', (req, res) => {
     res.send(
