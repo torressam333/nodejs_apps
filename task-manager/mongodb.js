125f0c9c2dd9fa22f4f5ac002becd872a06665bc
Samuel <torressam333@gmail.com>
2021-01-28 07:08:36 -0700

    Reading documents from mongodb

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -6,10 +6,6 @@ const ObjectID = mongodb.ObjectID;*/
 
 const {MongoClient, ObjectID} = require('mongodb');
 
-const id = new ObjectID();
-console.log(id);
-console.log(id.getTimestamp());
-
 //Define connection url for the DB we are connecting to
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -21,40 +17,18 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
             return console.log('Unable to connect to db: ' + error);
         }
 
-        //1. Reference specific db to manipulate
+        //1. Reference specific db to manipulate (connection)
         const db = client.db(databaseName);
 
-        //2. Insert 3 tasks into a new collection (tasks)
-      /*  db.collection('tasks').insertMany([
-            {
-                description: 'Read for 30 minutes',
-                completed: false
-            },
-            {
-                description: 'Practice piano for 30 minutes',
-                completed: false
-            },
-            {
-                description: 'Study node.js and mongodb',
-                completed: true
-            }
-        ], (error, result) => {
+        db.collection('tasks').findOne({ _id: new ObjectID("600f3302e5ffbbaff2c161ae")}, (error, task) => {
             if (error) {
-                return console.log(error.message);
+                return console.log('Unable to fetch');
             }
 
-            console.log(result.ops);
-        });*/
+            console.log(task);
+        });
 
-        //3. Insert single user
-        /*db.collection('users').insertOne({
-            name: 'Robert',
-            age: 21
-        }, (error, result) => {
-            if (error) {
-                return console.log('Something went wrong ' + error);
-            }
-
-            console.log(result.ops);
-        })*/
+        db.collection('tasks').find({completed: false}).toArray((error, incompleteTasks) => {
+            console.log(incompleteTasks);
+        });
     });
