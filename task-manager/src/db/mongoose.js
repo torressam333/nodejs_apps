2840d39337ec2edb8cc162309a88f8b90a044fb5
Samuel <torressam333@gmail.com>
2021-02-04 07:13:47 -0700

    Implement and test validation and sanitization for user model

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
 
@@ -12,13 +13,32 @@ mongoose.connect(connectionURL, {
 const User = mongoose.model('User', {
     name: {
         type: String,
+        required: true,
+        trim: true
     },
     age: {
-        typeof: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid');
+            }
+        }
     }
 });
 
-const sam = new User({
-    name: 'Sam',
-    age: '28'
+const Rob = new User({
+    name: '  Robby  ',
+    email: '  ROB@GMAIL.COM  '
 }).save().then(res =>console.log(res)).catch(err => console.log(err));
