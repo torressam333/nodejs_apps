e4ce4b8d63571a0069307bd48d016077a08dda01
Samuel <torressam333@gmail.com>
2021-02-16 19:07:11 -0700

    Set up endpoints for fetching all users and individual users by id

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //Auto parse incoming json to an object
-app.use(express.json())
+app.use(express.json());
 
 //Create user endpoint
 app.post('/users', (req, res) => {
@@ -34,12 +34,35 @@ app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
     //persist task
     task.save().then(() => {
-        res.status(201.).send(task);
+        res.status(201).send(task);
     }).catch((err) => {
         res.status(400).send(err.message);
     });
 });
 
+
+//Fetch all users using Mongoose
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users) //sends back all users in response
+    }).catch((error) => {
+        res.status(500).send(error.message);
+    })
+});
+
+//Find user by id
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(400).send('User not found');
+        }
+
+        res.send(user);
+
+    }).catch(error => res.status(500).send(error.message));
+});
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
