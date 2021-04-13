510a7e708af6ee472dcda877533dd25964151d66
Samuel <torressam333@gmail.com>
2021-04-13 06:37:31 -0700

    Add cascading deletes for users and their tasks

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -84,6 +85,15 @@ userSchema.pre('save', async function (next) {
 
     //Ensure process is completed
     next();
+});
+
+//Cascade delete user tasks when user is deleted
+userSchema.pre('remove', async function (next) {
+   const user = this;
+
+   await Task.deleteMany({ owner: user._id});
+
+   next();
 });
 
 /*Reusable method to generate JWT for users*/
