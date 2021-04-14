e70e3902fec09d01432bb2339ad0bb827f01f656
Samuel <torressam333@gmail.com>
2021-04-14 06:35:42 -0700

    Add proper timestamps to user and task schema

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -1,22 +1,25 @@
 const mongoose = require('mongoose');
 
-const Task = mongoose.model('Task', {
-   description: {
-       type: String,
-       required: true,
-       trim: true,
-   },
+const taskSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
     completed: {
-       type: Boolean,
+        type: Boolean,
         default: false
     },
     owner: {
-       type: mongoose.Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         required: true,
         ref: 'User'
     }
+}, {
+    timestamps: true
 });
 
+const Task = mongoose.model('Task', taskSchema);
 
 
 module.exports = Task;
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -50,6 +50,9 @@ const userSchema = new mongoose.Schema({
             }
         }
     ]
+}, {
+    //Schema options
+    timestamps: true
 });
 
 /*Virtual property
